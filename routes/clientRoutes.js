124886a530960d3de755c197cd2d4952d1a5a91d
Prabhanshu Lakshakar <lakshakarprabhanshu@gmail.com>
2025-07-25 13:17:21 +0530

    Update clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,17 +1,13 @@
+// Clean and fixed version:
 const express = require("express");
 const router = express.Router();
-
+const clientController = require("../controllers/clientController");
 const { verifyToken } = require("../middlewares/auth");
 const { checkRole } = require("../middlewares/roleCheck");
-const clientController = require("../controllers/clientController");
 
-
-// Create a new booking
-router.post("/bookings", verifyToken, bookingController.createBooking);
-
-
-// GET /api/client/bookings?status=pending
-router.get('/bookings', verifyToken, clientController.getClientBookings);
+// Use same name in routes
+router.post("/bookings", verifyToken, clientController.createBooking);
+router.get("/bookings", verifyToken, clientController.getClientBookings);
 router.delete("/bookings/:id", verifyToken, checkRole("client"), clientController.cancelBooking);
 
 module.exports = router;
