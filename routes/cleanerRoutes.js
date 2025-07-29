d229eec7a7a52796a49d738f972b267bad868f6d
Prabhanshu Lakshakar <lakshakarprabhanshu@gmail.com>
2025-07-29 21:08:01 +0530

    Update cleanerRoutes.js

--- routes/cleanerRoutes.js
+++ routes/cleanerRoutes.js
@@ -7,4 +7,10 @@ const { checkRole } = require("../middlewares/roleCheck");
 router.get("/jobs", verifyToken, checkRole("cleaner"), cleanerController.getAvailableJobs);
 router.post("/jobs/:id/accept", verifyToken, checkRole("cleaner"), cleanerController.acceptJob);
 
+// Only for logged-in cleaners
+router.get("/bookings", verifyToken, checkRole("cleaner"), cleanerController.getAssignedBookings);
+
+router.put("/bookings/:id/status", verifyToken, checkRole("cleaner"), cleanerController.updateBookingStatus);
+
+
 module.exports = router;
